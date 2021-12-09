90f5c787e469dc3e532bc5172d47165442c263e9
Henrique Gogó <henriquegogo@gmail.com>
2021-12-08 23:44:58 -0300

    chroot scripts

---
+++ scripts/chrooter/start.sh
@@ -0,0 +1,1 @@
+sudo chroot ./filesystem
---
+++ scripts/chrooter/umount.sh
@@ -0,0 +1,15 @@
+sudo umount ./filesystem/root
+sudo umount ./filesystem/tmp
+sudo umount ./filesystem/var
+sudo umount ./filesystem/proc
+sudo umount ./filesystem/sys
+sudo umount ./filesystem/dev/pts
+sudo umount ./filesystem/dev/shm
+sudo umount ./filesystem/dev
+sudo umount ./filesystem/var/run/udev
+sudo umount ./filesystem/var/run
+sudo umount ./filesystem/run/udev
+sudo umount ./filesystem/run
+sudo umount ./filesystem
+sudo umount ./filesystem
+sudo rm ./root ./tmp ./var -rf
